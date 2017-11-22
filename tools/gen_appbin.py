ee5f3bda0f6e25681493ea055574dc08c8e87dc1
Ben Stuart <mail@bstuart.de>
2017-11-22 16:27:27 +0100

    Pep8 python code, hardcoded used python interpreter

--- tools/gen_appbin.py
+++ tools/gen_appbin.py
@@ -40,82 +40,85 @@ CHECKSUM_INIT = 0xEF
 chk_sum = CHECKSUM_INIT
 blocks = 0
 
-def write_file(file_name,data):
-	if file_name is None:
-		print 'file_name cannot be none\n'
-		sys.exit(0)
 
-	fp = open(file_name,'ab')
+def write_file(file_name, data):
+    if file_name is None:
+        print('file_name cannot be none\n')
+        sys.exit(0)
 
-	if fp:
-		fp.seek(0,os.SEEK_END)
-		fp.write(data)
-		fp.close()
-	else:
-		print '%s write fail\n'%(file_name)
+    fp = open(file_name, 'ab')
 
-def combine_bin(file_name,dest_file_name,start_offset_addr,need_chk):
+    if fp:
+        fp.seek(0, os.SEEK_END)
+        fp.write(data)
+        fp.close()
+    else:
+        print('%s write fail\n' % (file_name))
+
+
+def combine_bin(file_name, dest_file_name, start_offset_addr, need_chk):
     global chk_sum
     global blocks
     if dest_file_name is None:
-        print 'dest_file_name cannot be none\n'
+        print('dest_file_name cannot be none\n')
         sys.exit(0)
 
     if file_name:
-        fp = open(file_name,'rb')
+        fp = open(file_name, 'rb')
         if fp:
-        	########## write text ##########
-            fp.seek(0,os.SEEK_END)
+            ########## write text ##########
+            fp.seek(0, os.SEEK_END)
             data_len = fp.tell()
             if data_len:
-		if need_chk:
+                if need_chk:
                     tmp_len = (data_len + 3) & (~3)
-		else:
-	            tmp_len = (data_len + 15) & (~15)
-                data_bin = struct.pack('<II',start_offset_addr,tmp_len)
-                write_file(dest_file_name,data_bin)
-                fp.seek(0,os.SEEK_SET)
+                else:
+                    tmp_len = (data_len + 15) & (~15)
+                data_bin = struct.pack('<II', start_offset_addr, tmp_len)
+                write_file(dest_file_name, data_bin)
+                fp.seek(0, os.SEEK_SET)
                 data_bin = fp.read(data_len)
-                write_file(dest_file_name,data_bin)
-		if need_chk:
-		    for loop in range(len(data_bin)):
-		        chk_sum ^= ord(data_bin[loop])
+                write_file(dest_file_name, data_bin)
+                if need_chk:
+                    for loop in range(len(data_bin)):
+                        chk_sum ^= ord(data_bin[loop])
                 # print '%s size is %d(0x%x),align 4 bytes,\nultimate size is %d(0x%x)'%(file_name,data_len,data_len,tmp_len,tmp_len)
                 tmp_len = tmp_len - data_len
                 if tmp_len:
-                    data_str = ['00']*(tmp_len)
+                    data_str = ['00'] * (tmp_len)
                     data_bin = binascii.a2b_hex(''.join(data_str))
-                    write_file(dest_file_name,data_bin)
-		    if need_chk:
-			for loop in range(len(data_bin)):
-			    chk_sum ^= ord(data_bin[loop])
+                    write_file(dest_file_name, data_bin)
+                    if need_chk:
+                        for loop in range(len(data_bin)):
+                            chk_sum ^= ord(data_bin[loop])
                 blocks = blocks + 1
-        	fp.close()
+                fp.close()
         else:
-        	print '!!!Open %s fail!!!'%(file_name)
+            print('!!!Open %s fail!!!' % (file_name))
 
 
-def getFileCRC(_path): 
-    try: 
-        blocksize = 1024 * 64 
-        f = open(_path,"rb") 
-        str = f.read(blocksize) 
-        crc = 0 
-        while(len(str) != 0): 
-            crc = binascii.crc32(str, crc) 
-            str = f.read(blocksize) 
-        f.close() 
-    except: 
-        print 'get file crc error!' 
-        return 0 
+def getFileCRC(_path):
+    try:
+        blocksize = 1024 * 64
+        f = open(_path, "rb")
+        str = f.read(blocksize)
+        crc = 0
+        while(len(str) != 0):
+            crc = binascii.crc32(str, crc)
+            str = f.read(blocksize)
+        f.close()
+    except:
+        print('get file crc error!')
+        return 0
     return crc
+
 
 def gen_appbin():
     global chk_sum
     global crc_sum
     global blocks
     if len(sys.argv) != 6:
-        print 'Usage: gen_appbin.py eagle.app.out boot_mode flash_mode flash_clk_div flash_size_map'
+        print('Usage: gen_appbin.py eagle.app.out boot_mode flash_mode flash_clk_div flash_size_map')
         sys.exit(0)
 
     elf_file = sys.argv[1]
@@ -124,30 +127,30 @@ def gen_appbin():
     flash_clk_div = sys.argv[4]
     flash_size_map = sys.argv[5]
 
-    flash_data_line  = 16
+    flash_data_line = 16
     data_line_bits = 0xf
 
     irom0text_bin_name = 'eagle.app.v6.irom0text.bin'
     text_bin_name = 'eagle.app.v6.text.bin'
     data_bin_name = 'eagle.app.v6.data.bin'
     rodata_bin_name = 'eagle.app.v6.rodata.bin'
-    flash_bin_name ='eagle.app.flash.bin'
+    flash_bin_name = 'eagle.app.flash.bin'
 
-    BIN_MAGIC_FLASH  = 0xE9
-    BIN_MAGIC_IROM   = 0xEA
+    BIN_MAGIC_FLASH = 0xE9
+    BIN_MAGIC_IROM = 0xEA
     data_str = ''
     sum_size = 0
 
-    if os.getenv('COMPILE')=='xcc' :
+    if os.getenv('COMPILE') == 'xcc':
         cmd = 'xt-nm -g ' + elf_file + ' > eagle.app.sym'
-    else :
+    else:
         cmd = 'xtensa-lx106-elf-nm -g ' + elf_file + ' > eagle.app.sym'
 
     os.system(cmd)
 
     fp = file('./eagle.app.sym')
     if fp is None:
-        print "open sym file error\n"
+        print("open sym file error\n")
         sys.exit(0)
 
     lines = fp.readlines()
@@ -162,7 +165,7 @@ def gen_appbin():
             # print entry_addr
 
     if entry_addr is None:
-        print 'no entry point!!'
+        print('no entry point!!')
         sys.exit(0)
 
     data_start_addr = '0'
@@ -185,19 +188,19 @@ def gen_appbin():
     #============================
     #  SPI FLASH PARAMS
     #-------------------
-    #flash_mode=
+    # flash_mode=
     #     0: QIO
     #     1: QOUT
     #     2: DIO
     #     3: DOUT
     #-------------------
-    #flash_clk_div=
+    # flash_clk_div=
     #     0 :  80m / 2
     #     1 :  80m / 3
     #     2 :  80m / 4
     #    0xf:  80m / 1
     #-------------------
-    #flash_size_map=
+    # flash_size_map=
     #     0 : 512 KB (256 KB + 256 KB)
     #     1 : 256 KB
     #     2 : 1024 KB (512 KB + 512 KB)
@@ -208,65 +211,69 @@ def gen_appbin():
     #-------------------
     #   END OF SPI FLASH PARAMS
     #============================
-    byte2=int(flash_mode)&0xff
-    byte3=(((int(flash_size_map)<<4)| int(flash_clk_div))&0xff)
-	
+    byte2 = int(flash_mode) & 0xff
+    byte3 = (((int(flash_size_map) << 4) | int(flash_clk_div)) & 0xff)
+
     if boot_mode == '2':
         # write irom bin head
-        data_bin = struct.pack('<BBBBI',BIN_MAGIC_IROM,4,byte2,byte3,long(entry_addr,16))
+        data_bin = struct.pack('<BBBBI', BIN_MAGIC_IROM,
+                               4, byte2, byte3, int(entry_addr, 16))
         sum_size = len(data_bin)
-        write_file(flash_bin_name,data_bin)
-        
-        # irom0.text.bin
-        combine_bin(irom0text_bin_name,flash_bin_name,0x0,0)
+        write_file(flash_bin_name, data_bin)
 
-    data_bin = struct.pack('<BBBBI',BIN_MAGIC_FLASH,3,byte2,byte3,long(entry_addr,16))
+        # irom0.text.bin
+        combine_bin(irom0text_bin_name, flash_bin_name, 0x0, 0)
+
+    data_bin = struct.pack('<BBBBI', BIN_MAGIC_FLASH, 3,
+                           byte2, byte3, int(entry_addr, 16))
     sum_size = len(data_bin)
-    write_file(flash_bin_name,data_bin)
+    write_file(flash_bin_name, data_bin)
 
     # text.bin
-    combine_bin(text_bin_name,flash_bin_name,TEXT_ADDRESS,1)
+    combine_bin(text_bin_name, flash_bin_name, TEXT_ADDRESS, 1)
 
     # data.bin
     if data_start_addr:
-        combine_bin(data_bin_name,flash_bin_name,long(data_start_addr,16),1)
+        combine_bin(data_bin_name, flash_bin_name, int(data_start_addr, 16), 1)
 
     # rodata.bin
-    combine_bin(rodata_bin_name,flash_bin_name,long(rodata_start_addr,16),1)
+    combine_bin(rodata_bin_name, flash_bin_name, int(rodata_start_addr, 16), 1)
 
     # write checksum header
     sum_size = os.path.getsize(flash_bin_name) + 1
-    sum_size = flash_data_line - (data_line_bits&sum_size)
+    sum_size = flash_data_line - (data_line_bits & sum_size)
     if sum_size:
-        data_str = ['00']*(sum_size)
+        data_str = ['00'] * (sum_size)
         data_bin = binascii.a2b_hex(''.join(data_str))
-        write_file(flash_bin_name,data_bin)
-    write_file(flash_bin_name,chr(chk_sum & 0xFF))
-    	
+        write_file(flash_bin_name, data_bin)
+    write_file(flash_bin_name, chr(chk_sum & 0xFF))
+
     if boot_mode == '1':
         sum_size = os.path.getsize(flash_bin_name)
-        data_str = ['FF']*(0x10000-sum_size)
+        data_str = ['FF'] * (0x10000 - sum_size)
         data_bin = binascii.a2b_hex(''.join(data_str))
-        write_file(flash_bin_name,data_bin)
+        write_file(flash_bin_name, data_bin)
 
-        fp = open(irom0text_bin_name,'rb')
+        fp = open(irom0text_bin_name, 'rb')
         if fp:
             data_bin = fp.read()
-            write_file(flash_bin_name,data_bin)
+            write_file(flash_bin_name, data_bin)
             fp.close()
-        else :
-            print '!!!Open %s fail!!!'%(flash_bin_name)
+        else:
+            print('!!!Open %s fail!!!' % (flash_bin_name))
             sys.exit(0)
     if boot_mode == '1' or boot_mode == '2':
         all_bin_crc = getFileCRC(flash_bin_name)
         if all_bin_crc < 0:
             all_bin_crc = abs(all_bin_crc) - 1
-        else :
+        else:
             all_bin_crc = abs(all_bin_crc) + 1
-        print "bin crc: %x"%all_bin_crc
-        write_file(flash_bin_name,chr((all_bin_crc & 0x000000FF))+chr((all_bin_crc & 0x0000FF00) >> 8)+chr((all_bin_crc & 0x00FF0000) >> 16)+chr((all_bin_crc & 0xFF000000) >> 24))
+        print("bin crc: %x" % all_bin_crc)
+        write_file(flash_bin_name, chr((all_bin_crc & 0x000000FF)) + chr((all_bin_crc & 0x0000FF00)
+                                                                         >> 8) + chr((all_bin_crc & 0x00FF0000) >> 16) + chr((all_bin_crc & 0xFF000000) >> 24))
     cmd = 'rm eagle.app.sym'
     os.system(cmd)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     gen_appbin()
